896b61cb7edbca94fba0e7f87896380985ee372d
AlekseyM <maryaskin_av@srvhub.ru>
2020-11-21 21:27:41 +0700

    Update prod settings

--- portfolio/settings/prod_settings.py
+++ portfolio/settings/prod_settings.py
@@ -1,7 +1,14 @@
 DEBUG = False
 
 SECRET_KEY = ')gj8u2&jmv(zt%n=z7r&z-y(l!q2(zlvv=_d0j7qheacql7)d^'
-ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = [".myskill.site", ".myskill.site."]
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'info@myskill.site'
+EMAIL_HOST_PASSWORD = 'Dima'
 
 DATABASES = {
     'default': {
